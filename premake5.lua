78cc23d61992e4e75958205d2a8d9d6545dd93f5
gnireorb <62901166+gnireorb@users.noreply.github.com>
2022-01-16 06:18:52 -0300

    imgui, json and spdlog

--- premake5.lua
+++ premake5.lua
@@ -6,25 +6,29 @@ workspace ("suburbs")
    defines { "_CRT_SECURE_NO_WARNINGS" }
    configurations { "Debug", "Release" }
 
+project ("imgui")
+   location ("include/%{prj.name}")
+   kind ("StaticLib")
+   language ("C++")
+   targetdir ("bin/%{cfg.buildcfg}")
+   includedirs { "include/%{prj.name}" }
+   files { "include/%{prj.name}/backends/imgui_impl_dx9.cpp", "include/%{prj.name}/backends/imgui_impl_win32.cpp", "include/%{prj.name}/imgui.cpp", "include/%{prj.name}/imgui_tables.cpp", "include/%{prj.name}/imgui_demo.cpp", "include/%{prj.name}/imgui_widgets.cpp", "include/%{prj.name}/imgui_draw.cpp", "include/%{prj.name}/misc/cpp/imgui_stdlib.cpp" }
+
 project ("suburbs")
    location ("suburbs")
-   
    kind ("ConsoleApp")
    language ("C++")
-
    targetdir ("bin/%{cfg.buildcfg}")
    objdir ("bin/obj/%{cfg.buildcfg}/%{prj.name}")
 
    disablewarnings { "4244" }
-
    PrecompiledHeaderInclude = "common.hpp"
    PrecompiledHeaderSource = "%{prj.name}/src/common.cpp"
-   
-   includedirs { "%{prj.name}/src" }
 
-   links { "ImGui" }
-
+   includedirs { "%{prj.name}/src", "include/json/single_include", "include/spdlog/include", "include/imgui", "include/imgui/backends" }
    files { "%{prj.name}/src/**.cpp", "%{prj.name}/src/**.hpp" }
+
+   links { "imgui" }
    
    filter "configurations:Debug"
       defines { "DEBUG" }
