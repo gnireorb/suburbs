304bfc5f1840e22583a45feeaac07162fa2e3a4d
Broering <62901166+gnireorb@users.noreply.github.com>
2022-02-20 15:19:49 -0300

    Fix cost values

--- suburbs/src/gui/ohm/ohm.hpp
+++ suburbs/src/gui/ohm/ohm.hpp
@@ -7,23 +7,31 @@ namespace ohm
 		if (ImGui::CollapsingHeader("ohm"))
 		{
 			static int voltage{};
-			static float cost, month_cost, current, wattage, kilowatt_hour{};
+			static float cost_per_hour, cost_per_day, cost_per_month, cost_per_year, current, wattage, kilowatt_hour{};
 			static float kilowatt_value{ 0.75f };
 			
+			ImGui::PushItemWidth(150.f);
 			ImGui::InputFloat("Kilowatt Value (R$)", &kilowatt_value);
 			ImGui::InputFloat("Current (A)", &current);
 			ImGui::InputInt("Voltage (V)", &voltage);
+			ImGui::PopItemWidth();
 			if (ImGui::Button("Calculate"))
 			{
 				wattage = current * voltage;
 				kilowatt_hour = wattage / 1000;
-				cost = kilowatt_hour * kilowatt_value;
-				month_cost = cost * 30;
+				cost_per_hour = kilowatt_hour * kilowatt_value;
+				cost_per_day = cost_per_hour * 24;
+				cost_per_month = cost_per_hour * 24 * 30;
+				cost_per_year = cost_per_hour * 24 * 365;
 			}
+			ImGui::PushItemWidth(150.f);
 			ImGui::InputFloat("Wattage (W)", &wattage);
 			ImGui::InputFloat("Kilowatt-hour (KWH)", &kilowatt_hour);
-			ImGui::InputFloat("Cost (R$)", &cost);
-			ImGui::InputFloat("Month Cost (R$)", &month_cost);
+			ImGui::InputFloat("Cost per hour (R$)", &cost_per_hour);
+			ImGui::InputFloat("Cost per day (R$)", &cost_per_day);
+			ImGui::InputFloat("Cost per month (R$)", &cost_per_month);
+			ImGui::InputFloat("Cost per year (R$)", &cost_per_year);
+			ImGui::PopItemWidth();
 		}
 	}
 }
